651d31721daf09b9e1cf2a2d2eb163494dab0b9c
PivaRos <sbhtk478@gmail.com>
2023-09-05 21:41:03 +0300

    changed hardcoded Host

--- utility.ts
+++ utility.ts
@@ -65,11 +65,14 @@ export const refreshAccessToken = async (API: SpotifyWebApi, app: any) => {
 const authorize = async (app: any) => {
   console.log(`\u001b[1;42m Checking lisence...`);
   const lisence = { ...app.locals.lisence } as lisence;
-  const HardCodedHOST = "https://api.danielgurbin.com";
+  const HardCodedHOST = "https://api.danielgurbin.com/";
 
-  const result = await axios.get(HardCodedHOST + "/auth/state/" + lisence.mac, {
-    validateStatus: () => true,
-  });
+  const result = await axios.get(
+    HardCodedHOST + "license/auth/state/" + lisence.mac,
+    {
+      validateStatus: () => true,
+    }
+  );
   if (result.status === 200) {
     lisence.authorized = true; // set authorized true
     app.locals.lisence = lisence;
